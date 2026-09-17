00365ce85005a471d01d36e6cbf75f36f7167f7a
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-17 11:11:09 +0800

    fix: load battle stability layer before battle mode

--- public/main.js
+++ public/main.js
@@ -27,6 +27,7 @@ const XIUXIAN_FEATURE_MODULES = [
   './cultivation/material-system.js',
   './cultivation/cultivation-refinery-v2.js',
   './cultivation/material-drop-system.js',
+  './cultivation/battle-v3-stability-ui.js',
   './cultivation/battle-mode-v2.js',
   './cultivation/cultivation-status-panel.js',
   './cultivation/realm-breakthrough-feedback.js',
