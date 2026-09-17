16686cba4cd914e42137cee833402b142ba464fb
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-17 13:01:28 +0800

    fix: load high content layout guard

--- public/main.js
+++ public/main.js
@@ -29,6 +29,7 @@ const XIUXIAN_FEATURE_MODULES = [
   './cultivation/training-fluid-layout.js',
   './cultivation/market-fluid-layout.js',
   './cultivation/material-realm-ui.js',
+  './cultivation/content-capacity-layout.js',
   './cultivation/material-drop-system.js',
   './cultivation/battle-v3-stability-ui.js',
   './cultivation/battle-mode-v2.js',
