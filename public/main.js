92d27571e822036ee86491b971432886a696eca1
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-21 13:27:06 +0800

    Invalidate browser cache to load corrected complete refinery recipe stylesheet

--- public/main.js
+++ public/main.js
@@ -66,7 +66,7 @@ let xiuxianFeatureLoadStarted = false;
 let xiuxianReadyTimer = null;
 let resolveXiuxianFeatureGate;
 const xiuxianFeatureGate = new Promise((resolve) => { resolveXiuxianFeatureGate = resolve; });
-const XIUXIAN_FEATURE_BUILD = '20260921-recipe-expand2';
+const XIUXIAN_FEATURE_BUILD = '20260921-recipe-style-fix3';
 
 // 交易市集僅為可選功能，載入失敗不可阻止玩家登入或進入遊戲。
 let xiuxianMarketLoad = null;
