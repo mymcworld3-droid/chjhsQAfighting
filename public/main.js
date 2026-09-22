a0af53d6427f0ac3915f656ccddb0840b5532869
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-22 16:08:21 +0800

    Refresh app and modules after exposing Status from first login

--- public/main.js
+++ public/main.js
@@ -70,7 +70,7 @@ let xiuxianFeatureLoadStarted = false;
 let xiuxianReadyTimer = null;
 let resolveXiuxianFeatureGate;
 const xiuxianFeatureGate = new Promise((resolve) => { resolveXiuxianFeatureGate = resolve; });
-const XIUXIAN_FEATURE_BUILD = '20260922-core-layout2';
+const XIUXIAN_FEATURE_BUILD = '20260922-status-from-start1';
 
 // 交易市集僅為可選功能，載入失敗不可阻止玩家登入或進入遊戲。
 let xiuxianMarketLoad = null;
