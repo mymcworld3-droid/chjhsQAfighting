24988f21aec862de5672b34367616c72f1103e5f
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-22 09:04:15 +0800

    Finish cinematic battle scene assets and refresh cached client build

--- public/main.js
+++ public/main.js
@@ -69,7 +69,7 @@ let xiuxianFeatureLoadStarted = false;
 let xiuxianReadyTimer = null;
 let resolveXiuxianFeatureGate;
 const xiuxianFeatureGate = new Promise((resolve) => { resolveXiuxianFeatureGate = resolve; });
-const XIUXIAN_FEATURE_BUILD = '20260921-battle-absolute-clock2';
+const XIUXIAN_FEATURE_BUILD = '20260922-duel-cinematic1';
 
 // 交易市集僅為可選功能，載入失敗不可阻止玩家登入或進入遊戲。
 let xiuxianMarketLoad = null;
