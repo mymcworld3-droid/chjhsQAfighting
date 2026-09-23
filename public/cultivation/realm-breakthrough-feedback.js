fddb9f057a84885565925ad3ca37b9751529628d
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-23 14:06:01 +0800

    refactor: rename 登仙 realm to 半仙

--- public/cultivation/realm-breakthrough-feedback.js
+++ public/cultivation/realm-breakthrough-feedback.js
@@ -24,7 +24,7 @@
     { name: '合體', sub: '天地合一', need: 308 },
     { name: '大乘', sub: '大道將成', need: 448 },
     { name: '渡劫', sub: '雷劫問道', need: 628 },
-    { name: '登仙', sub: '仙門在望', need: 868 },
+    { name: '半仙', sub: '仙門在望', need: 868 },
     { name: '真仙', sub: '榜上仙位', need: 868 }
   ];
 
