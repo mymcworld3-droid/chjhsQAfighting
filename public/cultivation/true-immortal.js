fa5cfa33e492af58fe724c5eab5a583e451888c9
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-24 16:12:37 +0800

    Require updated half-immortal threshold for true immortal rank

--- public/cultivation/true-immortal.js
+++ public/cultivation/true-immortal.js
@@ -1,4 +1,4 @@
-// 868 修為只能進入「半仙」；只有同時持有九州五大仙榜有效仙位者才顯示為「真仙」。
+// 2588 修為只能進入「半仙」；只有同時持有九州五大仙榜有效仙位者才顯示為「真仙」。
 (function () {
   const roles = new Set(['ru-xian', 'fa-xian', 'suan-xian', 'xuan-xian', 'wai-xian']);
   let members = new Set();
@@ -9,7 +9,7 @@
     window.dispatchEvent(new Event('xiuxian:immortals-updated'));
   };
   window.isTrueImmortal = (score, uid = window.getRealmUserUid?.()) =>
-    window.trueImmortalBoardReady && Number(score) >= 868 && !!uid && members.has(uid);
+    window.trueImmortalBoardReady && Number(score) >= 2588 && !!uid && members.has(uid);
   window.limitImmortalRank = (rank, score, realms, uid = window.getRealmUserUid?.()) => {
     const immortal = realms.findIndex(realm => realm.name === '真仙');
     return immortal >= 0 && rank >= immortal && !window.isTrueImmortal(score, uid)
