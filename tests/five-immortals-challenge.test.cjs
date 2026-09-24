bbdb802066c37a39201edaef084ab9c16eff62df
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-24 16:14:39 +0800

    Align late realm eligibility, storyline and equipment test thresholds

--- tests/five-immortals-challenge.test.cjs
+++ tests/five-immortals-challenge.test.cjs
@@ -5,8 +5,8 @@ const { join } = require('node:path');
 
 const source = readFileSync(join(__dirname, '../public/cultivation/five-immortals.js'), 'utf8');
 
-test('Five Immortals requires Tribulation realm at 628 cultivation', () => {
-  assert.match(source, /const TRIBULATION_SCORE = 628/);
+test('Five Immortals requires Tribulation realm at 1868 cultivation', () => {
+  assert.match(source, /const TRIBULATION_SCORE = 1868/);
   assert.match(source, /currentScore\(\) >= TRIBULATION_SCORE/);
   assert.match(source, /latestUser\.stats\?\.totalScore[\s\S]*TRIBULATION_SCORE/);
 });
