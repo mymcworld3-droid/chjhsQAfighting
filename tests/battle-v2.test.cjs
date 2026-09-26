ebe7ec4d88fecd996ff7b454b43f69ce35194bff
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-26 21:46:34 +0800

    test: include nascent seal label in battle harness

--- tests/battle-v2.test.cjs
+++ tests/battle-v2.test.cjs
@@ -328,7 +328,7 @@ test('formal battle uses Foundation realm only; incomplete tutorials never block
     toast: () => calls.push('toast'), alert: () => calls.push('alert'),
     resetRuntime: () => calls.push('reset'), ensurePage: () => calls.push('ensurePage'),
     showSection: () => calls.push('lobby'), renderLobby: () => calls.push('renderLobby'),
-    playerSnapshot: () => ({name:'p'}), setText:()=>{}, playerCoreLabel:()=> '', playerPowerLabel:()=> '', setPlayerAvatar:()=>{},
+    playerSnapshot: () => ({name:'p'}), setText:()=>{}, playerCoreLabel:()=> '', playerPowerLabel:()=> '', playerNascentSealLabel:()=> '', setPlayerAvatar:()=>{},
     findAndClaimRoom: async () => 'oldRoom', subscribeRoom: () => calls.push('subscribe'),
     createWaitingRoom: async () => {calls.push('newRoom');return 'newRoom';},
     scheduleReconcile:()=>{}, console,
