528eb09564d98b59d6f151aafff7789268826c19
mymcworld3-droid <mymcworld3@gmail.com>
2026-09-26 20:10:18 +0800

    chore: bump feature build for multiplayer raid

--- public/main.js
+++ public/main.js
@@ -77,7 +77,7 @@ let xiuxianFeatureLoadStarted = false;
 let xiuxianReadyTimer = null;
 let resolveXiuxianFeatureGate;
 const xiuxianFeatureGate = new Promise((resolve) => { resolveXiuxianFeatureGate = resolve; });
-const XIUXIAN_FEATURE_BUILD = '20260926-item-images1';
+const XIUXIAN_FEATURE_BUILD = '20260926-raid-multiplayer1';
 
 // 交易市集僅為可選功能，載入失敗不可阻止玩家登入或進入遊戲。
 let xiuxianMarketLoad = null;
